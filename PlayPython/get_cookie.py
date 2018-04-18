5c8fd6e8c7a01f2d36a9ac710f3c077a5f6dddee
ddpicc <cuiqian1988@gmail.com>
2018-04-18 15:43:22 +0800

    Update get_cookie.py

--- PlayPython/get_cookie.py
+++ PlayPython/get_cookie.py
@@ -1,13 +1,14 @@
 from selenium import webdriver
 import time
 import os
-def get_cookie_from_network(url_login):
+
+def wtCookieToFileFromNetwork(url_login, account_name, password):
     url_login = 'https://www.douban.com/login' 
     driver = webdriver.Edge()
     time.sleep(3)
     driver.get(url_login)
-    driver.find_element_by_xpath('//input[@type="text"]').send_keys('cuiqian1988@gmail.com') # 改成你的微博账号
-    driver.find_element_by_xpath('//input[@type="password"]').send_keys('cnaiq1988') # 改成你的微博密码
+    driver.find_element_by_xpath('//input[@type="text"]').send_keys(account_name) # 改成你的微博账号
+    driver.find_element_by_xpath('//input[@type="password"]').send_keys(password) # 改成你的微博密码
     driver.find_element_by_xpath('//input[@type="submit"]').click() # 点击登录
     time.sleep(3)
     # 获得 cookie信息
@@ -20,4 +21,4 @@ def get_cookie_from_network(url_login):
         f.seek(-1 ,os.SEEK_END)
         f.truncate()
 
-driver.close()
+    driver.close()
